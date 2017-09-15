db60610b30507faa144c1963e6fbc03b936db0de
HenryBarker <henry.barker@standardbank.co.za>
2017-09-15 14:41:59 +0200

    add and play with angular gridster2

--- src/ProjectBlanket.WebNew/src/app/components/dashboard/dashboard.component.ts
+++ src/ProjectBlanket.WebNew/src/app/components/dashboard/dashboard.component.ts
@@ -3,62 +3,122 @@ import { Component, OnInit, OnChanges } from '@angular/core'
 import { QuoteService } from '../../services/quote.service';
 import { Global } from '../../shared/global';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-//import Quoteservice = require("../../services/quote.service");
-
 
 @Component({
-   
-    templateUrl: 'dashboard.component.html'
+  selector: 'dashboard',
+  templateUrl: 'dashboard.component.html'
 })
 
 export class DashboardComponent {//implements OnInit {
 
   options: GridsterConfig;
   dashboard: Array<GridsterItem>;
-  
-  
-  
+
+
+
   constructor(private fb: FormBuilder, private _quoteService: QuoteService) { }
-  
+
   itemChange(item: any, itemComponent: any) {
-      console.info('itemChanged', item, itemComponent);
+    console.info('itemChanged', item, itemComponent);
   }
-  
+
   itemResize(item: any, itemComponent: any) {
-      console.info('itemResized', item, itemComponent);
+    console.info('itemResized', item, itemComponent);
   }
-  
+
   ngOnInit() {
-      this.options = {
-          itemChangeCallback: this.itemChange,
-          itemResizeCallback: this.itemResize,
-      };
-  
-      this.dashboard = [
-          { cols: 2, rows: 1, y: 0, x: 0 },
-          { cols: 2, rows: 2, y: 0, x: 2 }
-      ];
+    this.options = {
+      itemChangeCallback: this.itemChange,
+      itemResizeCallback: this.itemResize,
+
+      gridType: 'fit',
+      compactType: 'none',
+      //  itemInitCallback: DashboardComponent.itemInit,
+      margin: 5,
+      outerMargin: true,
+      mobileBreakpoint: 640,
+      minCols: 1,
+      maxCols: 100,
+      minRows: 1,
+      maxRows: 100,
+      maxItemCols: 100,
+      minItemCols: 1,
+      maxItemRows: 100,
+      minItemRows: 1,
+      maxItemArea: 2500,
+      minItemArea: 1,
+      defaultItemCols: 1,
+      defaultItemRows: 1,
+      fixedColWidth: 105,
+      fixedRowHeight: 105,
+      keepFixedHeightInMobile: false,
+      keepFixedWidthInMobile: false,
+      scrollSensitivity: 10,
+      scrollSpeed: 20,
+      enableEmptyCellClick: false,
+      enableEmptyCellContextMenu: false,
+      enableEmptyCellDrop: false,
+      enableEmptyCellDrag: false,
+      //  emptyCellClickCallback: this.emptyCellClick.bind(this),
+      // emptyCellContextMenuCallback: this.emptyCellClick.bind(this),
+      // emptyCellDropCallback: this.emptyCellClick.bind(this),
+      // emptyCellDragCallback: this.emptyCellClick.bind(this),
+      emptyCellDragMaxCols: 50,
+      emptyCellDragMaxRows: 50,
+      draggable: {
+        enabled: true,
+        ignoreContentClass: 'gridster-item-content',
+        ignoreContent: false,
+        dragHandleClass: 'drag-handler',
+        // stop: AppComponent.eventStop
+      },
+      resizable: {
+        enabled: true,
+        //  stop: AppComponent.eventStop,
+        handles: {
+          s: true,
+          e: true,
+          n: true,
+          w: true,
+          se: true,
+          ne: true,
+          sw: true,
+          nw: true
+        }
+      },
+      api: {
+        //   resize: AppComponent.eventStop,
+        //  optionsChanged: AppComponent.eventStop,
+        //  getNextPossiblePosition: AppComponent.eventStop,
+      },
+      swap: false,
+      pushItems: true,
+      pushResizeItems: false,
+      displayGrid: 'onDrag&Resize',
+      disableWindowResize: false
+    };
+
+    this.dashboard = [
+      { cols: 2, rows: 1, y: 0, x: 0 },
+      { cols: 2, rows: 2, y: 0, x: 2 },
+      { cols: 2, rows: 1, y: 0, x: 0 },
+      { cols: 2, rows: 2, y: 0, x: 2 },
+      { cols: 2, rows: 1, y: 0, x: 0 },
+      { cols: 2, rows: 2, y: 0, x: 2 }
+    ];
   }
-  
+
   changedOptions() {
-      this.options.api.optionsChanged();
+    this.options.api.optionsChanged();
   }
-  
+
   removeItem(item: any) {
-      this.dashboard.splice(this.dashboard.indexOf(item), 1);
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
   }
-  
+
   addItem() {
-      this.dashboard.push({});
+    this.dashboard.push({});
   }
 
 
 }
-//}
-
-
-
-
-
-
-
